56caddc5d826e18cc50e752c5a7d4b9402b207a0
Aman Kumar <114167696+roger7242@users.noreply.github.com>
2025-08-21 14:56:27 +0530

    Update streamlit_app.py to add new menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,10 @@ import streamlit as st
 
 
 #enabling multiple pages 
-main_page = st.Page("main_page.py", title="Menu", icon="🍲")
+main_page = st.Page("main_page.py", title="Home", icon=":->")
 page_2 = st.Page("baskets.py", title="Baskets", icon="🧺")
 page_3 = st.Page("stocks.py", title="Stocks", icon="🥕")
+page_3 = st.Page("menu.py", title="Menu", icon="🍲")
 
 # Set up navigation
 pg = st.navigation([main_page, page_2, page_3])
