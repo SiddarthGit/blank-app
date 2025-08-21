cdfe9011983e05940471b206cc3f56cb591723e2
Aman Kumar <114167696+roger7242@users.noreply.github.com>
2025-08-21 14:57:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 #enabling multiple pages 
-main_page = st.Page("main_page.py", title="Home", icon=":->")
+main_page = st.Page("main_page.py", title="Home")
 page_2 = st.Page("baskets.py", title="Baskets", icon="🧺")
 page_3 = st.Page("stocks.py", title="Stocks", icon="🥕")
 page_3 = st.Page("menu.py", title="Menu", icon="🍲")
